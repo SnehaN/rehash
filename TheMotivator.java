d8ee89733cfed3f65c2e3c1bbe6a3841ddbfca7b
Sneha Narkar <yourfriend.sneha@gmail.com>
2016-04-07 16:03:38 -0700

    added event

--- TheMotivator.java
+++ TheMotivator.java
@@ -3,17 +3,31 @@ public class TheMotivator
 	public void scorefeedback(int score)
 	 {
 		if (score == 100)
+		{
 			System.out.println("You're awesome");
+			upcomingEvents();
+		}
 		else if (score > 90)
+		{
 			System.out.println("That's great");
+			upcomingEvents();
+		}
 		else if (score > 60)
 			System.out.println("That's good ");
 		else
 			System.out.println("Well, what can I say?");
 	}
+
+	public void upcomingEvents()
+	{
+		System.out.println("As you have scored more than 90 then you deserve a party!!!!");
+	}
+
+
 	public static void main(String[] args)
 	 {
 		TheMotivator tm = new TheMotivator();
-		tm.scorefeedback(60);
+		tm.scorefeedback(91);
+		
 	}
 }
